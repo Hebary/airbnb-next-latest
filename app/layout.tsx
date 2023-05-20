4326e52a94f00c7a06230b4f2450ed3287e098eb
shakharho <berdaddario@gmail.com>
2023-05-20 11:42:45 -0300

    feat: creating modal component and button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next'
 import './styles/globals.css'
 import { Nunito } from 'next/font/google'
 import { Navbar } from './components/navbar'
+import { Modal } from './components/modals'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
@@ -14,6 +15,11 @@ export default function RootLayout({ children }: {children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <Modal 
+            isOpen
+            title={'Login'}
+       
+          />
         <Navbar />
         {children}
       </body>
