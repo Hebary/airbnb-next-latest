17dcb7647c6d9547f769a6701f52d26de2b16f9b
shakharho <berdaddario@gmail.com>
2023-05-22 09:57:39 -0300

    feat: size modal fix

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -54,9 +54,9 @@ const Modal: FC<Props> = ({ isOpen, onClose, onSubmit, title, body, footer, acti
 
     return (
         <>
-            <div className='flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 backdrop-blur-sm'>
-                <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5  mx-auto h-full lg:h-auto md:h-auto scale-90'>
-                    <div className={` translate duration-300 h-full  ${showModal ? 'translate-y-0' : 'translate-y-full'} ${showModal ? 'opacity-100' : 'opacity-0'} `}>
+            <div className='flex justify-center md:py-5 items-center overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 backdrop-blur-sm'>
+                <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 mx-auto h-full lg:h-auto md:h-auto scale-90'>
+                    <div className={` translate duration-300 h-full ${showModal ? 'translate-y-0' : 'translate-y-full'} ${showModal ? 'opacity-100' : 'opacity-0'} `}>
                         <div className='translate h-auto rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
                             {/* HEADER */}
                             <div className='flex items-center p-6 rounded-t justify-center relative border-b-[1px]'>
